4e6eb7dc80f5397d1e275e75c4d592a9b12cc2e8
thami <thamiresthsvs@gmail.com>
2025-04-05 17:34:32 -0300

    App tempo - agenda 7

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using Microsoft.Maui.Networking;  // Para usar a verificação de conectividade
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,17 +13,58 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                // Verificar se há conexão com a internet
+                if (!Connectivity.NetworkAccess.HasFlag(NetworkAccess.Internet))
+                {
+                    lbl_res.Text = "Usuário sem conexão com a internet.";
+                    return;
+                }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                // Verificar se o campo cidade não está vazio
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    // Tenta obter a previsão para a cidade informada
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                    if (t != null)
+                    {
+                        // Se os dados da previsão foram encontrados
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Descricao: {t.description} \n" +
+                                         $"Velocidade: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    }
+                    else
+                    {
+                        // Se não foi possível obter a previsão para a cidade
+                        lbl_res.Text = "Cidade não localizada.";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Exceção inesperada, exibe mensagem de erro
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
-
 }
+
